215ffea79ed670aac7ac03e292eb82bebda3e3d0
sverrirhakonar <sverrirhak@gmail.com>
2025-10-07 22:44:41 -0500

    task 3 done

--- strategies.py
+++ strategies.py
@@ -8,10 +8,15 @@ class Strategy(ABC):
     def generate_signals(self, tick: MarketDataPoint) -> list:
         pass
 
+# Space Complexity: O(n)
+# The list self.prices grows indefinitely and linearly with terms of input size
 class NaiveMovingAverageStrategy(Strategy):
     def __init__(self):
         self.prices = []
     
+    # Time Complexity: O(n)
+    # The sum function for calculating the mean price iterates,
+    # over the entire list, and calculates the sum.
     def generate_signals(self, tick):
         price = tick.price
         if len(self.prices) == 0:
@@ -29,12 +34,18 @@ class NaiveMovingAverageStrategy(Strategy):
         return signal
     
 
+# Space Complexity: O(k), where k is the window size
+# Self.prices implements a deque that inserts and removes element, keeping the space fixed by the window size.
 class WindowedMovingAverageStrategy(Strategy):
     def __init__(self, window = 20):
         self.window = window
         self.prices = deque()
         self.running_sum = 0
     
+    # Time Complexity: O(1)
+    # The running sum keeps the sum, it updates by subtracting the removed value and adds 
+    # the most recent price to the sum, both at a constant O(1) time complexity.
+
     def generate_signals(self, tick):
         price = tick.price
         if len(self.prices) < self.window:
